5dd2b20d93660b4ea4885a9fd803ac40d00f247b
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-09-26 14:05:48 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TimelineModule } from 'primeng/timeline';     //accordion and accordion tab
+import { TimelineModule } from 'primeng/timeline';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +16,10 @@ import { TimelineModule } from 'primeng/timeline';     //accordion and accordion
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TimelineModule
+    TimelineModule,
+    CardModule,
+    ButtonModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
